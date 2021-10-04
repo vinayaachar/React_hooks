b2a635f7f575d9146fbae7f043004a989d3f4ebb
vinayaachar <vinayaachar@gmail.com>
2021-10-04 16:37:15 -0700

    useState hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FaStar } from 'react-icons/fa';
+
+const createArray = (length) => [
+  ...Array(length)
+]
+
+function Star({selected= false, onSelect}) {
+  return (
+    <FaStar
+      color = {selected ? "red" : "gray"}
+      onClick = {onSelect}
+    />
+  )
+}
+
+function StarRating({ totalStar = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStar).map((n, i) => (
+        <Star
+          key = {i}
+          selected= {selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+      />
+    ))}
+    <p>{selectedStars} of {totalStar}</p>
+
+    </>
+
+
+  )
+}
+
+
+function App() {
+  return <StarRating totalStar = {5} />
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name = "Vinay"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
